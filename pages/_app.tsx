d2115cadab5da96ca5f611d1a77e45fb4e3b2cc2
Tal Berenstein <zeevtorrestal@gmail.com>
2020-12-16 01:25:16 +0100

    index.tsx => HomeProps + React.FC (for Typescript)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.scss';
 import 'semantic-ui-css/semantic.min.css';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,47 +3,52 @@ import { Grid, Image } from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.scss';
 
-export default function Home() {
-  const indexImg = 'doormenora.jpeg';
-  const logoImg = 'logoOrakel.jpeg';
-  // const candleImg = 'ZINK.jpeg';
-  return (
-    <Layout title="Orakel Förlag - Home">
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <div>
-            <Image className={styles.logo} src={logoImg} size="tiny" />
-          </div>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={3}></Grid.Column>
-              <Grid.Column width={2}></Grid.Column>
-              <Grid.Column></Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={7}>
-                <Link href="/profetia">
-                  <h1 className={styles.title}>Profetia</h1>
-                </Link>
-                <h1>Böcker</h1>
-                <Link href="/forfattare">
-                  <h1 className={styles.title}>Författare</h1>
-                </Link>
-                <h1>Projekt</h1>
-                <Link href="/kontakt">
-                  <h1 className={styles.title}>Kontakt</h1>
-                </Link>
-                <h1>Manus</h1>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <div>
-                  <Image className={styles.imgIndex} src={indexImg} />
-                </div>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </main>
-      </div>
-    </Layout>
-  );
-}
+type HomeProps = {
+  logoImg: string;
+  indexImg: string;
+};
+
+const Home: React.FC<HomeProps> = ({
+  logoImg = 'logoOrakel.jpeg',
+  indexImg = 'doormenora.jpeg'
+}) => (
+  <Layout title="Orakel Förlag - Home">
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div>
+          <Image className={styles.logo} src={logoImg} size="tiny" />
+        </div>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}></Grid.Column>
+            <Grid.Column width={2}></Grid.Column>
+            <Grid.Column></Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={7}>
+              <Link href="/profetia">
+                <h1 className={styles.title}>Profetia</h1>
+              </Link>
+              <h1>Böcker</h1>
+              <Link href="/forfattare">
+                <h1 className={styles.title}>Författare</h1>
+              </Link>
+              <h1>Projekt</h1>
+              <Link href="/kontakt">
+                <h1 className={styles.title}>Kontakt</h1>
+              </Link>
+              <h1>Manus</h1>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <div>
+                <Image className={styles.imgIndex} src={indexImg} />
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </main>
+    </div>
+  </Layout>
+);
+
+export default Home;
